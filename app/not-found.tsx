17e4302b4f535c0afc7f64a985cbbde6e83e8f42
Ryan Carpenter <scrian_12@yahoo.com>
2024-03-27 17:32:35 -0400

    fixed not found page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -5,14 +5,16 @@ import { FaExclamationTriangle } from 'react-icons/fa';
 
 const NotFoundPage = () => {
   return (
-    <section className='min-h-screen grow bg-blue-50'>
-      <div className='container m-auto max-w-2xl py-24'>
-        <div className=' m-4 rounded-md border bg-background-2 px-6 py-24 shadow-md dark:bg-gray-800 md:m-0'>
+    <section className='h-screen grow  bg-background-2 dark:bg-gray-800'>
+      <div className='container mx-auto mt-[200px] max-w-2xl py-24'>
+        <div className='m-4 rounded-md border bg-white-800 px-6 py-24 shadow-lg md:m-0'>
           <div className='flex justify-center'>
             <FaExclamationTriangle className='fa fa-exclamation-triangle fa-5x text-8xl text-yellow-400' />
           </div>
           <div className='text-center'>
-            <h1 className='mb-2 mt-4 text-3xl font-bold'>Page Not Found</h1>
+            <h1 className='text-black-800 mb-2 mt-4 text-3xl font-bold'>
+              Page Not Found
+            </h1>
             <p className='mb-10 text-xl text-gray-500'>
               The page you are looking for does not exist.
             </p>
