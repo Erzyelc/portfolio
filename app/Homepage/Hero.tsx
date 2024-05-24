6ad66e3a0209f45342557a01f3083f11c68390b4
Ryan Carpenter <scrian_12@yahoo.com>
2024-05-24 12:59:19 -0400

    added icons

--- app/Homepage/Hero.tsx
+++ app/Homepage/Hero.tsx
@@ -111,6 +111,14 @@ const Hero = () => {
               className='object-contain'
             />
           </section>
+
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            viewBox='0 0 384 512'
+            className=' mt-20 h-10 w-10 animate-bounce dark:fill-white-800 sm:hidden'
+          >
+            <path d='M169.4 502.6c12.5 12.5 32.8 12.5 45.3 0l128-128c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 402.7 224 32c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 370.7L86.6 329.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l128 128z' />
+          </svg>
         </section>
       </motion.div>
     </motion.div>
--- app/layout.tsx
+++ app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang='en'>
+    <html lang='en' suppressHydrationWarning>
       <body className={`${poppins.className} bg-background-2 dark:bg-gray-800`}>
         <Providers>
           <NavBar />
